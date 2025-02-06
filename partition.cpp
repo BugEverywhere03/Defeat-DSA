fca627efaf2cea7ed92b519b4fce8a0f93144cab
BugEveryWhere03 <duongdevpython@gmail.com>
2025-02-06 09:47:41 +0700

    Update partition.cpp
    Update DP Solution

--- partition.cpp
+++ partition.cpp
@@ -32,12 +32,52 @@ void backtrack(vector<vector<string>> &result, string s, vector<string> &par, in
         }
     }
 }
+void backtrackoptimize(vector<vector<string>> &result, vector<vector<bool>> &dp, vector<string> &par, int start, string s)
+{
+    if (start == s.length())
+    {
+        result.push_back(par);
+        return;
+    }
+
+    for (int i = start; i < s.length(); i++)
+    {
+        if (dp[start][i])
+        {
+            par.push_back(s.substr(start, i - start + 1));
+            backtrackoptimize(result, dp, par, i + 1, s);
+            par.pop_back();
+        }
+    }
+}
 
 vector<vector<string>> partition(string s)
 {
     vector<vector<string>> result;
     vector<string> par;
-    backtrack(result, s, par, 0);
+    int n = s.length();
+    vector<vector<bool>> dp(n, vector<bool>(n, false));
+    // Gán các chuỗi có độ dài 1 bằng True vì nó đối xứng
+    for (int i = 0; i < n; i++)
+    {
+        dp[i][i] = true;
+    }
+    // Gán gía trị cho các chuổi có độ dài lớn hơn 2
+    for (int length = 2; length <= n; ++length)
+    {
+        for (int i = 0; i <= n - length; ++i)
+        {
+            int j = i + length - 1;
+            if (length == 2)
+            {
+                dp[i][j] = (s[i] == s[j]);
+            }
+            else
+                dp[i][j] = (s[i] == s[j] && dp[i + 1][j - 1]);
+        }
+    }
+    // backtrack(result, s, par, 0);
+    backtrackoptimize(result, dp, par, 0, s);
     return result;
 }
 
